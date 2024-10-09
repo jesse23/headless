781dd86187f8dd885b00b0537547cc3634dc3749
jesse23 <vijcp@outlook.com>
2024-10-09 13:37:16 -0400

    update

--- apps/ts-sample/src/App.tsx
+++ apps/ts-sample/src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import {
   CommandExample,
@@ -13,26 +11,15 @@ function App() {
   const enableLocalSample = true;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       {enableLocalSample && (
         <>
+          <ComponentDeclExample />
           <PanelExample />
           <FormExample />
         </>
       )}
       <CommandExample />
       <LoadPanelExample />
-      <ComponentDeclExample />
     </>
   );
 }
